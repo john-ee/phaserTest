9587d006f45466d8b6c77049f03f99d1948bf95a
Hill John-Nathan <john.nat.hill@gmail.com>
2017-03-09 20:11:38 +0100

    Initialisation

---
+++ src/player.js
@@ -0,0 +1,12 @@
+const default_position = 19;
+var direction;
+var velocity = 100;
+var left = 28;
+var right = 10;
+var up = 1;
+var down = 19;
+
+function Player(game){
+    this.game = game;
+    this.sprite = null;
+}
